a7b3d680add10f918dce4dc5a18ede6056e07a36
usernameMaciej <maciej.przystarz@protonmail.com>
2023-05-22 19:19:14 +0200

    stworzenie projektu

---
+++ src/main/java/com/example/weatherapiexercise/WeatherApiExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.weatherapiexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherApiExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherApiExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/weatherapiexercise/WeatherApiExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weatherapiexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApiExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
